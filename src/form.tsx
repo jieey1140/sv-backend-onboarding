f38eb972a00700bc057a82bfe182a05e744aaf24
jieey1140 <85172908+jieey1140@users.noreply.github.com>
2022-01-07 13:55:40 +0900

    fest: form des write

--- src/form.tsx
+++ src/form.tsx
@@ -1,38 +1,17 @@
 import {
   Form,
-  Select,
-  InputNumber,
-  Switch,
   Radio,
-  Slider,
   Button,
-  Upload,
-  Rate,
-  Checkbox,
-  Row,
-  Col,
 } from 'antd';
-import { UploadOutlined, InboxOutlined } from '@ant-design/icons';
-
-const { Option } = Select;
 
 const formItemLayout = {
   labelCol: { span: 6 },
   wrapperCol: { span: 14 },
 };
 
-const normFile = (e: any) => {
-  console.log('Upload event:', e);
-  if (Array.isArray(e)) {
-    return e;
-  }
-  return e && e.fileList;
-};
-
 export default function FORM() {
   return (
-    <>
-          <Form
+    <Form
       name="validate_other"
       {...formItemLayout}
       initialValues={{
@@ -41,148 +20,58 @@ export default function FORM() {
         rate: 3.5,
       }}
     >
-      <Form.Item label="Plain Text">
-        <span className="ant-form-text">China</span>
-      </Form.Item>
-      <Form.Item
-        name="select"
-        label="Select"
-        hasFeedback
-        rules={[{ required: true, message: 'Please select your country!' }]}
-      >
-        <Select placeholder="Please select a country">
-          <Option value="china">China</Option>
-          <Option value="usa">U.S.A</Option>
-        </Select>
-      </Form.Item>
-
-      <Form.Item
-        name="select-multiple"
-        label="Select[multiple]"
-        rules={[{ required: true, message: 'Please select your favourite colors!', type: 'array' }]}
-      >
-        <Select mode="multiple" placeholder="Please select favourite colors">
-          <Option value="red">Red</Option>
-          <Option value="green">Green</Option>
-          <Option value="blue">Blue</Option>
-        </Select>
-      </Form.Item>
-
-      <Form.Item label="InputNumber">
-        <Form.Item name="input-number" noStyle>
-          <InputNumber min={1} max={10} />
-        </Form.Item>
-        <span className="ant-form-text"> machines</span>
-      </Form.Item>
-
-      <Form.Item name="switch" label="Switch" valuePropName="checked">
-        <Switch />
-      </Form.Item>
-
-      <Form.Item name="slider" label="Slider">
-        <Slider
-          marks={{
-            0: 'A',
-            20: 'B',
-            40: 'C',
-            60: 'D',
-            80: 'E',
-            100: 'F',
-          }}
-        />
-      </Form.Item>
-
-      <Form.Item name="radio-group" label="Radio.Group">
+      <Form.Item name="radio-group" label="평소에 PC 게임을 즐겨하십니까?">
         <Radio.Group>
-          <Radio value="a">item 1</Radio>
-          <Radio value="b">item 2</Radio>
-          <Radio value="c">item 3</Radio>
+          <Radio value="a">예</Radio>
+          <Radio value="b">아니요</Radio>
         </Radio.Group>
       </Form.Item>
 
-      <Form.Item
-        name="radio-button"
-        label="Radio.Button"
-        rules={[{ required: true, message: 'Please pick an item!' }]}
-      >
+      <Form.Item name="radio-group" label="평소에 모바일 게임을 즐겨하십니까?">
         <Radio.Group>
-          <Radio.Button value="a">item 1</Radio.Button>
-          <Radio.Button value="b">item 2</Radio.Button>
-          <Radio.Button value="c">item 3</Radio.Button>
+          <Radio value="a">예</Radio>
+          <Radio value="b">아니요</Radio>
         </Radio.Group>
       </Form.Item>
 
-      <Form.Item name="checkbox-group" label="Checkbox.Group">
-        <Checkbox.Group>
-          <Row>
-            <Col span={8}>
-              <Checkbox value="A" style={{ lineHeight: '32px' }}>
-                A
-              </Checkbox>
-            </Col>
-            <Col span={8}>
-              <Checkbox value="B" style={{ lineHeight: '32px' }} disabled>
-                B
-              </Checkbox>
-            </Col>
-            <Col span={8}>
-              <Checkbox value="C" style={{ lineHeight: '32px' }}>
-                C
-              </Checkbox>
-            </Col>
-            <Col span={8}>
-              <Checkbox value="D" style={{ lineHeight: '32px' }}>
-                D
-              </Checkbox>
-            </Col>
-            <Col span={8}>
-              <Checkbox value="E" style={{ lineHeight: '32px' }}>
-                E
-              </Checkbox>
-            </Col>
-            <Col span={8}>
-              <Checkbox value="F" style={{ lineHeight: '32px' }}>
-                F
-              </Checkbox>
-            </Col>
-          </Row>
-        </Checkbox.Group>
+      <Form.Item name="radio-group" label="일주일에 며칠 정도 PC 게임을 하십니까?">
+        <Radio.Group>
+          <Radio value="a">1일 미만</Radio>
+          <Radio value="b">1~2일</Radio>
+          <Radio value="c">3~4일</Radio>
+          <Radio value="d">5~7일</Radio>
+          <Radio value="e">2주 간 거의 매일</Radio>
+        </Radio.Group>
       </Form.Item>
 
-      <Form.Item name="rate" label="Rate">
-        <Rate />
+      <Form.Item name="radio-group" label="일주일에 며칠 정도 모바일게임을 하십니까?">
+        <Radio.Group>
+          <Radio value="a">1일 미만</Radio>
+          <Radio value="b">1~2일</Radio>
+          <Radio value="c">3~4일</Radio>
+          <Radio value="d">5~7일</Radio>
+          <Radio value="e">2주 간 거의 매일</Radio>
+        </Radio.Group>
       </Form.Item>
 
-      <Form.Item
-        name="upload"
-        label="Upload"
-        valuePropName="fileList"
-        getValueFromEvent={normFile}
-        extra="longgggggggggggggggggggggggggggggggggg"
-      >
-        <Upload name="logo" action="/upload.do" listType="picture">
-          <Button icon={<UploadOutlined />}>Click to upload</Button>
-        </Upload>
-      </Form.Item>
-
-      <Form.Item label="Dragger">
-        <Form.Item name="dragger" valuePropName="fileList" getValueFromEvent={normFile} noStyle>
-          <Upload.Dragger name="files" action="/upload.do">
-            <p className="ant-upload-drag-icon">
-              <InboxOutlined />
-            </p>
-            <p className="ant-upload-text">Click or drag file to this area to upload</p>
-            <p className="ant-upload-hint">Support for a single or bulk upload.</p>
-          </Upload.Dragger>
-        </Form.Item>
+      <Form.Item name="radio-group" label="지난 일주일 동안 게임 때문에 밤을 샌 적이 있다.">
+        <Radio.Group>
+          <Radio value="a">0일</Radio>
+          <Radio value="b">1일</Radio>
+          <Radio value="c">2일</Radio>
+          <Radio value="d">3일</Radio>
+          <Radio value="e">4일</Radio>
+          <Radio value="f">5일</Radio>
+          <Radio value="g">6일</Radio>
+          <Radio value="h">7일</Radio>
+        </Radio.Group>
       </Form.Item>
 
       <Form.Item wrapperCol={{ span: 12, offset: 6 }}>
         <Button type="primary" htmlType="submit">
-          Submit
+          제출
         </Button>
       </Form.Item>
     </Form>
-    </>
   )
 }
